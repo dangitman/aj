d996f4a21e5b16a98a70259a4a387f2105fcbb44
aperras824 <dyslexic96@gmail.com>
2013-07-26 15:05:45 -0400

    Not extending Thread

--- src/com/aj/project/Project.java
+++ src/com/aj/project/Project.java
@@ -3,7 +3,7 @@ package com.aj.project;
  * The Project
  * @author Dan Perras
  */
-public class Project extends Thread {
+public class Project {
 	
 	boolean isRunning;
 	
@@ -25,19 +25,15 @@ public class Project extends Thread {
 		while(isRunning){
 			System.out.println("running");
 			try {
-				Project.sleep(5000);
+				Thread.sleep(5000);
 			} catch (InterruptedException e) {
 
 				e.printStackTrace();
 			}
 		}
-		
-		//TODO make a while loop tests a CLASS-LEVEL boolean variable called isRunning
-		//once start is called, that variable will need to be set to true before entering the while loop
-		//in the while loop just print "running" every 5 seconds
 	}
 	
-	//public void stop() {
-		//isRunning = false;
-	//}
+	public void stop() {
+		isRunning = false;
+	}
 }
