77d103cc6b22064bfab82d59367891a02f785e1c
Dan <danp84@gmail.com>
2013-07-26 13:21:46 -0400

    asd

---
+++ src/com/aj/project/Project.java
@@ -0,0 +1,21 @@
+package com.aj.project;
+/**
+ * The Project
+ * @author Dan Perras
+ */
+public class Project {
+
+	/**
+	 * Create a new {@link Project}
+	 */
+	public Project() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Start the project!
+	 */
+	public void start() {
+		//do something eventually
+	}
+}
---
+++ src/com/aj/project/ProjectService.java
@@ -0,0 +1,17 @@
+package com.aj.project;
+
+/**
+ * Main entry point into our Project
+ * @author Dan Perras
+ */
+public class ProjectService {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//create and start the project!
+
+	}
+
+}
