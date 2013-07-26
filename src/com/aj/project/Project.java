9bbe25b3c2d914aca12bf81ded648bbaa4f6fbe2
Dan <danp84@gmail.com>
2013-07-26 14:39:08 -0400

    as

--- src/com/aj/project/Project.java
+++ src/com/aj/project/Project.java
@@ -3,8 +3,7 @@ package com.aj.project;
  * The Project
  * @author Dan Perras
  */
-public class Project extends Thread {
-
+public class Project {
 	/**
 	 * Create a new {@link Project}
 	 */
@@ -16,6 +15,15 @@ public class Project extends Thread {
 	 * Start the project!
 	 */
 	public void start() {
+		//TODO print something about the program is now started.
 		System.out.println("Doing something");
+		
+		//TODO make a while loop tests a CLASS-LEVEL boolean variable called isRunning
+		//once start is called, that variable will need to be set to true before entering the while loop
+		//in the while loop just print "running" every 5 seconds
+	}
+	
+	public void stop() {
+		//TODO what needs to be done here to stop the while loop?
 	}
 }
--- src/com/aj/project/ProjectService.java
+++ src/com/aj/project/ProjectService.java
@@ -10,6 +10,7 @@ public class ProjectService {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//TODO project will need to be run within it's own thread
 		Project p = new Project();
 		p.start();
 
